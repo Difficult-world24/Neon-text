f03c550ce44ea75505c91f3e210200a0d021f721
Kartik Sharma <kartiksharma@Kartiks-MacBook-Air.local>
2022-09-21 11:15:23 +0530

    Basic Things up and running

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, TextField, Typography } from "@mui/material";
+import { Stack, Box } from "@mui/system";
+import interact from "interactjs";
+import "./App.css";
 
 function App() {
+  const [pictureText, setPictureText] = useState("Hello World");
+  const [position,setPosition] = useState({ x: 0, y: 0 })
+
+
+  const handleInputChange = (evt) => {
+    const text = evt.target.value;
+    setPictureText(text);
+  };
+
+  const restrictToParent = interact.modifiers.restrictRect({
+    restriction: "parent",
+  });
+
+  interact(".ghost").draggable({
+    modifiers: [restrictToParent],
+    listeners: {
+      move(event) {
+        position.x += event.dx;
+        position.y += event.dy;
+
+        event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
+      },
+      end(event){
+        console.log(event.dx);
+        console.log(event.dy);
+      }
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box
+      sx={{
+        bgcolor: 'info.main',
+        marginTop: 2,
+      }}
+    >
+      <Stack
+        sx={{
+          direction: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        direction="row"
+        spacing={2}
+      >
+        <Box
+          sx={{
+            width: 500,
+            height: 281,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography
+            className="ghost neonText"
+            variant="h4"
+            sx={{
+              position: "absolute",
+            }}
+          >
+            {pictureText}
+          </Typography>
+          <img
+            width={"100%"}
+            alt="PlaceHolder"
+            src="https://wallpapercave.com/wp/wp6852847.jpg"
+          />
+        </Box>
+        <Stack direction="column" spacing={2}>
+          <TextField
+            id="standard-basic"
+            onChange={handleInputChange}
+            name="text"
+            label="Text For Picture"
+            variant="outlined"
+          />
+
+        </Stack>
+      </Stack>
+    </Box>
   );
 }
 
